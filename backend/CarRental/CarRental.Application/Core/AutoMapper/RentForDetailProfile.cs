943713336b237457eb452d06e0329a83594d29de
GracjanB <45687438+GracjanB@users.noreply.github.com>
2022-06-17 20:12:59 +0200

    Automapper profile for rent details

---
+++ backend/CarRental/CarRental.Application/Core/AutoMapper/RentForDetailProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using CarRental.Domain.Dtos;
+
+namespace CarRental.Application.Core.AutoMapper
+{
+    public class RentForDetailProfile : Profile
+    {
+        public RentForDetailProfile()
+        {
+            CreateMap<Domain.Entity.Rent, RentDetailsDto>()
+                .ForMember(dest => dest.Id, options => options.MapFrom(src => src.Id))
+                .ForMember(dest => dest.StartRent, options => options.MapFrom(src => src.StartRent))
+                .ForMember(dest => dest.EndRent, options => options.MapFrom(src => src.EndRent))
+                .ForMember(dest => dest.TotalPrice, options => options.MapFrom(src => src.TotalPrice))
+                .ForMember(dest => dest.Discount, options => options.MapFrom(src => src.Discount))
+                .ForMember(dest => dest.Status, options => options.MapFrom(src => src.Status))
+                .ForMember(dest => dest.User, options => options.MapFrom(src => src.User))
+                .ForMember(dest => dest.Vehicle, options => options.MapFrom(src => src.Vehicle));
+        }
+    }
+}
